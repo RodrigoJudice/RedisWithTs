9c38ed88ed2d0e7fda07f9fa86b692c0074db8f0
Judice <r_judice@msn.com>
2024-02-14 07:50:06 -0300

    Pipelining Commands

--- sandbox/index.ts
+++ sandbox/index.ts
@@ -1,5 +1,31 @@
 import 'dotenv/config';
 import { client } from '../src/services/redis';
+import { idText } from 'typescript';
 
-const run = async () => {};
+const run = async () => {
+	await client.hSet('car1', {
+		color: 'red',
+		year: 1950
+	});
+	await client.hSet('car2', {
+		color: 'green',
+		year: 1955
+	});
+	await client.hSet('car3', {
+		color: 'blue',
+		year: 1960
+	});
+
+	// const results = await Promise.all([
+	// 	client.hGetAll('car1'),
+	// 	client.hGetAll('car2'),
+	// 	client.hGetAll('car3')
+	// ]);
+
+	const commands = [1, 2, 3].map((id) => {
+		return client.hGetAll(`car${id}`);
+	});
+	const results = await Promise.all(commands);
+	console.log(results);
+};
 run();
--- src/services/queries/items/items.ts
+++ src/services/queries/items/items.ts
@@ -10,7 +10,13 @@ export const getItem = async (id: string) => {
 	return deserialize(id, item);
 };
 
-export const getItems = async (ids: string[]) => {};
+export const getItems = async (ids: string[]) => {
+	const commands = ids.map((id) => {
+		return client.hGetAll(itemsKey(id));
+	});
+	const results = await Promise.all(commands);
+	return results.map((result, i) => deserialize(ids[i], result));
+};
 
 export const createItem = async (attrs: CreateItemAttrs, userId: string) => {
 	const id = genId();
