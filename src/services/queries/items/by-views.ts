313a6d3689af08ef7171cee10e3725d866fdf10d
RodrigoJudice <r_judice@msn.com>
2024-02-20 10:17:51 -0300

    End Sort Command

--- src/services/queries/items/by-views.ts
+++ src/services/queries/items/by-views.ts
@@ -1,17 +1,33 @@
 import { client } from "$services/redis";
 import { itemsByViewsKey, itemsKey } from "$services/keys";
+import { deserialize } from "./deserialize";
 
 export const itemsByViews = async (order: 'DESC' | 'ASC' = 'DESC', offset = 0, count = 10) => {
-  const results = await client.sort(
+  let results:any = await client.sort(
     itemsByViewsKey(),
     {
       GET: [
         '#',
         `${itemsKey('*')}->name`,
-        `${itemsKey('*')}->views`
+        `${itemsKey('*')}->views`,
+        `${itemsKey('*')}->endingAt`,
+        `${itemsKey('*')}->imageUrl`,
+        `${itemsKey('*')}->price`,
       ],
-      BY: 'score'
+      BY: 'nosort',
+      DIRECTION: order,
+      LIMIT: [offset, count]
     }
   );
+
+  const items = [];
+  while (results.length) {
+    const [id, name, views, endingAt, imageUrl, price, ...rest] = results;
+    const item = deserialize( id, {name, views, endingAt, imageUrl, price} );
+    items.push(item);
+    results = rest;
+  }
+
+  return items;
 
 };
